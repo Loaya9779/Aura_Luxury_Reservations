658072e2498b958e30d60f440a6416b940abe862
Yousef <yusifkhaled16@gmail.com>
2026-07-12 19:40:47 +0300

    add listView for scrolling horizontally and vertically

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:aura_luxury_reservations/core/app_colors.dart';
 import 'package:aura_luxury_reservations/features/home/widgets/app_bar_widget.dart';
 import 'package:aura_luxury_reservations/features/home/widgets/home_title_widget.dart';
+import 'package:aura_luxury_reservations/features/home/widgets/restaurant_card_widget.dart';
 import 'package:aura_luxury_reservations/features/home/widgets/search_textfield_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -19,14 +20,32 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
+          scrollDirection: Axis.vertical,
           children: [
-            SearchTextField(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SearchTextField(),
 
-            SizedBox(height: height * 0.04),
-            HomeTitleWidget(width: width, height: height),
-            
+                SizedBox(height: height * 0.03),
+                HomeTitleWidget(width: width, height: height),
+                SizedBox(height: height * 0.03),
+                SizedBox(
+                  height: height * 0.35,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return RestaurantCardWidget(width: width, height: height);
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return SizedBox(width: width * 0.03);
+                    },
+                    itemCount: 4,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
