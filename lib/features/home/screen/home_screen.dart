13a6347f7ab7cc26d9756d10ee9d93f60c8e10c4
Yousef <yusifkhaled16@gmail.com>
2026-07-12 17:15:21 +0300

    implement appbar, search, and title

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,63 +1,35 @@
 import 'package:aura_luxury_reservations/core/app_colors.dart';
+import 'package:aura_luxury_reservations/features/home/widgets/app_bar_widget.dart';
+import 'package:aura_luxury_reservations/features/home/widgets/home_title_widget.dart';
+import 'package:aura_luxury_reservations/features/home/widgets/search_textfield_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppColors.surface,
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 18),
-          child: Container(
-            padding: const EdgeInsets.all(2),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: AppColors.primary.withValues(alpha: 0.25),
-                width: 1,
-              ),
-            ),
-            child: const CircleAvatar(
-              backgroundImage: AssetImage('assets/images/splash.png'),
-            ),
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications_none_rounded,
-                color: AppColors.primary,
-                size: 30,
-              ),
-            ),
-          ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: Size(1, 15),
-          child: Container(
-            color: AppColors.primary.withValues(alpha: 0.15),
-            height: 1,
-          ),
-        ),
-        backgroundColor: AppColors.surface,
-        title: Text(
-          'LUMIÈRE',
-          style: GoogleFonts.hankenGrotesk(
-            color: AppColors.primary,
-            fontSize: 32,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 4,
-          ),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(65),
+        child: AppBarWidget(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SearchTextField(),
+
+            SizedBox(height: height * 0.04),
+            HomeTitleWidget(width: width, height: height),
+            
+          ],
         ),
       ),
-      body: const Center(child: Text('Home')),
     );
   }
 }
