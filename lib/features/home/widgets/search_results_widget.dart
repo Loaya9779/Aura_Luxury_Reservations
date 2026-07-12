11ec438c672893f6690e1aed9de1ea51db235b8f
Yousef <yusifkhaled16@gmail.com>
2026-07-13 02:06:23 +0300

    finalize search results

--- lib/features/home/widgets/search_results_widget.dart
+++ lib/features/home/widgets/search_results_widget.dart
@@ -1,6 +1,5 @@
 import 'package:aura_luxury_reservations/core/app_colors.dart';
 import 'package:aura_luxury_reservations/features/home/cubit/home_cubit.dart';
-import 'package:aura_luxury_reservations/features/home/cubit/satates.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
