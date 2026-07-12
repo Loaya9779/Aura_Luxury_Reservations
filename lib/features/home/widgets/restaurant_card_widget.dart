5695d8bdb166da418aa6cf286c45d7bbb5e8a921
Yousef <yusifkhaled16@gmail.com>
2026-07-12 19:40:27 +0300

    add clickable card for retaurant details

--- lib/features/home/widgets/restaurant_card_widget.dart
+++ lib/features/home/widgets/restaurant_card_widget.dart
@@ -13,86 +13,89 @@ class RestaurantCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: width * 0.8,
-          height: height * 0.35,
-          decoration: BoxDecoration(
-            color: AppColors.primary,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(25),
-            child: Image.asset(
-              "assets/images/splash.png",
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {},
+      child: Stack(
+        children: [
+          Container(
+            width: width * 0.8,
+            height: height * 0.35,
+            decoration: BoxDecoration(
+              color: AppColors.primary,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Image.asset(
+                "assets/images/splash.png",
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Positioned(
-          bottom: height * 0.03,
-          left: width * 0.05,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    "4.5",
-                    style: TextStyle(
+          Positioned(
+            bottom: height * 0.03,
+            left: width * 0.05,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "4.5",
+                      style: TextStyle(
+                        color: AppColors.primary,
+                        fontSize: width * 0.04,
+                      ),
+                    ),
+                    SizedBox(width: width * 0.01),
+                    Icon(
+                      Icons.star_outlined,
                       color: AppColors.primary,
-                      fontSize: width * 0.04,
+                      size: width * 0.04,
                     ),
-                  ),
-                  SizedBox(width: width * 0.01),
-                  Icon(
-                    Icons.star_outlined,
-                    color: AppColors.primary,
-                    size: width * 0.04,
-                  ),
-                ],
-              ),
-              SizedBox(width: width * 0.01),
-              Text(
-                "L'Oiseau Bleu",
-                style: TextStyle(
-                  color: AppColors.white,
-                  fontSize: width * 0.06,
-                  fontWeight: FontWeight.w500,
+                  ],
                 ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Modern French",
-                    style: TextStyle(
-                      color: AppColors.textSecondary,
-                      fontSize: width * 0.04,
-                    ),
+                SizedBox(width: width * 0.01),
+                Text(
+                  "L'Oiseau Bleu",
+                  style: TextStyle(
+                    color: AppColors.white,
+                    fontSize: width * 0.06,
+                    fontWeight: FontWeight.w500,
                   ),
-                  SizedBox(width: width * 0.01),
-                  Text(
-                    "•",
-                    style: TextStyle(
-                      color: AppColors.textSecondary,
-                      fontSize: width * 0.04,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "Modern French",
+                      style: TextStyle(
+                        color: AppColors.textSecondary,
+                        fontSize: width * 0.04,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: width * 0.01),
-                  Text(
-                    "Paris",
-                    style: TextStyle(
-                      color: AppColors.textSecondary,
-                      fontSize: width * 0.04,
+                    SizedBox(width: width * 0.01),
+                    Text(
+                      "•",
+                      style: TextStyle(
+                        color: AppColors.textSecondary,
+                        fontSize: width * 0.04,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(width: width * 0.01),
+                    Text(
+                      "Paris",
+                      style: TextStyle(
+                        color: AppColors.textSecondary,
+                        fontSize: width * 0.04,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
