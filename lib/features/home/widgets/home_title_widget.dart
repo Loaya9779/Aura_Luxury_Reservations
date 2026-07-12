0892cd2d2682f5aed610cbea6abb317b77e17f50
Yousef <yusifkhaled16@gmail.com>
2026-07-12 17:14:43 +0300

    Home Title Custom Widget

---
+++ lib/features/home/widgets/home_title_widget.dart
@@ -0,0 +1,52 @@
+import 'package:aura_luxury_reservations/core/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeTitleWidget extends StatelessWidget {
+  const HomeTitleWidget({
+    super.key,
+    required this.width,
+    required this.height,
+  });
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Featured For You",
+          style: TextStyle(
+            color: AppColors.white,
+            fontSize: width * 0.06,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+    
+        SizedBox(height: height * 0.01),
+        Row(
+          children: [
+            Text(
+              "Selected for your exquisite taste",
+              style: TextStyle(
+                color: AppColors.textSecondary,
+                fontSize: width * 0.04,
+                // fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Spacer(),
+            Text(
+              "View All",
+              style: TextStyle(
+                color: AppColors.primary,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
