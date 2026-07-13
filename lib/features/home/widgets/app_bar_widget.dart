235540b4cf91436f55dfb7d8e199a0e8b76d3585
Yousef <yusifkhaled16@gmail.com>
2026-07-13 23:48:43 +0300

    app bar modifications

--- lib/features/home/widgets/app_bar_widget.dart
+++ lib/features/home/widgets/app_bar_widget.dart
@@ -3,41 +3,83 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppBarWidget extends StatelessWidget {
-  const AppBarWidget({super.key});
+  final bool isHome;
+  const AppBarWidget({super.key, required this.isHome});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: Padding(
-        padding: const EdgeInsets.only(left: 18),
-        child: Container(
-          padding: const EdgeInsets.all(2),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: AppColors.primary.withValues(alpha: 0.25),
-              width: 1,
+      leading: isHome
+          ? Padding(
+              padding: const EdgeInsets.only(left: 18),
+              child: Container(
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.primary.withValues(alpha: 0.25),
+                    width: 1,
+                  ),
+                ),
+                child: const CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/splash.png'),
+                ),
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.only(left: 18),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.popAndPushNamed(context, '/home');
+                },
+                icon: Icon(Icons.arrow_back_ios_new_outlined),
+                color: AppColors.primary,
+              ),
             ),
-          ),
-          child: const CircleAvatar(
-            backgroundImage: AssetImage('assets/images/splash.png'),
-          ),
-        ),
-      ),
       actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 16.0),
-          child: IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "/my-bookings");
-            },
-            icon: const Icon(
-              Icons.notifications_none_rounded,
-              color: AppColors.primary,
-              size: 30,
-            ),
-          ),
-        ),
+        isHome
+            ? Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.notifications_none_rounded,
+                    color: AppColors.primary,
+                    size: 30,
+                  ),
+                ),
+              )
+            : Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.notifications_none_rounded,
+                        color: AppColors.primary,
+                        size: 30,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: AppColors.primary.withValues(alpha: 0.25),
+                          width: 1,
+                        ),
+                      ),
+
+                      child: const CircleAvatar(
+                        radius: 15,
+                        backgroundImage: AssetImage('assets/images/splash.png'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ],
       bottom: PreferredSize(
         preferredSize: Size(1, 15),
@@ -47,15 +89,28 @@ class AppBarWidget extends StatelessWidget {
         ),
       ),
       backgroundColor: AppColors.surface,
-      title: Text(
-        'LUMIÈRE',
-        style: GoogleFonts.hankenGrotesk(
-          color: AppColors.primary,
-          fontSize: 32,
-          fontWeight: FontWeight.w500,
-          letterSpacing: 4,
-        ),
-      ),
+      title: isHome
+          ? Text(
+              'LUMIÈRE',
+              style: GoogleFonts.hankenGrotesk(
+                color: AppColors.primary,
+                fontSize: 32,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 4,
+              ),
+            )
+          : Align(
+              alignment: AlignmentGeometry.centerLeft,
+              child: Text(
+                'LUMIÈRE',
+                style: GoogleFonts.hankenGrotesk(
+                  color: AppColors.primary,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 4,
+                ),
+              ),
+            ),
     );
   }
 }
