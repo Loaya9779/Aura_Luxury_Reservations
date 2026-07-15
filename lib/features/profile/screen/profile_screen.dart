bf4fda753d913f5b2e7deeb7989d715984470c03
Yasser <mr.yasser82@gmail.com>
2026-07-16 02:50:52 +0300

    feat: profile picture widget ui design in profile screen

--- lib/features/profile/screen/profile_screen.dart
+++ lib/features/profile/screen/profile_screen.dart
@@ -1,3 +1,6 @@
+import 'package:aura_luxury_reservations/core/app_colors.dart';
+import 'package:aura_luxury_reservations/core/widgets/custom_app_bar.dart';
+import 'package:aura_luxury_reservations/features/profile/widgets/profile_picture_widget.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,6 +8,13 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: CustomAppBar(isCenter: true),
+      backgroundColor: AppColors.background,
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: Column(children: [ProfilePicture()]),
+      ),
+    );
   }
 }
