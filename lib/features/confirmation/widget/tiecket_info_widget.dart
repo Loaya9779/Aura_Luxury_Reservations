fc0f49ec21ef7d8583c130db5b21c649eaa4de0e
Yousef <yusifkhaled16@gmail.com>
2026-07-16 01:56:35 +0300

    ticket info custom widget

---
+++ lib/features/confirmation/widget/tiecket_info_widget.dart
@@ -0,0 +1,34 @@
+import 'package:aura_luxury_reservations/core/app_style.dart';
+import 'package:flutter/material.dart';
+
+class TicketInfoWidget extends StatelessWidget {
+  final String title;
+  final String info;
+
+  const TicketInfoWidget({
+    super.key,
+    required this.title,
+    required this.info,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          title,
+          style: AppStyle.bodySmall,
+        ),
+
+        const SizedBox(height: 6),
+
+        Text(
+          info,
+          style: AppStyle.headlineMedium,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
