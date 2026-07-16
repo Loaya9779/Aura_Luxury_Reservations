49263874a6337d7d95485a8783e3c93978f93fef
Yousef <yusifkhaled16@gmail.com>
2026-07-16 03:29:14 +0300

    update: add date and time in BookingCompleted State

--- lib/features/details_resturant/cubit/satates.dart
+++ lib/features/details_resturant/cubit/satates.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 abstract class BookingState {}
 
 class BookingInitial extends BookingState {}
@@ -6,7 +8,17 @@ class BookingUpdated extends BookingState {}
 
 class BookingLoading extends BookingState {}
 
-class BookingCompleted extends BookingState {}
+class BookingCompleted extends BookingState {
+  final DateTime date;
+  final TimeOfDay time;
+  final int guests;
+
+  BookingCompleted({
+    required this.date,
+    required this.time,
+    required this.guests,
+  });
+}
 
 class BookingError extends BookingState {
   final String error;
