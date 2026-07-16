c03ca5ea1ea258b230c5ab305d65a712a52dad37
Yasser <mr.yasser82@gmail.com>
2026-07-16 04:43:56 +0300

    feat: enhance profile screen layout and add booking history components with setting list tiles and finishing profile screen ui

---
+++ lib/features/profile/widgets/custom_list_tile.dart
@@ -0,0 +1,30 @@
+import 'package:aura_luxury_reservations/core/app_colors.dart';
+import 'package:aura_luxury_reservations/core/app_style.dart';
+import 'package:flutter/material.dart';
+
+class CustomListTile extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final bool isSignout;
+  const CustomListTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.isSignout,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon),
+      iconColor: isSignout ? AppColors.red : AppColors.outline,
+      titleTextStyle: isSignout
+          ? AppStyle.headlineExtraSmall.copyWith(color: AppColors.red)
+          : AppStyle.headlineExtraSmall,
+      title: Text(title),
+      trailing: isSignout ? null : Icon(Icons.keyboard_arrow_right),
+      contentPadding: EdgeInsets.zero,
+      onTap: () {},
+    );
+  }
+}
