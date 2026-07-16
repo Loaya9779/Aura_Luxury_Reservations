9e56a3b652eb8a00ecc7526f383534055bdce227
Yousef <yusifkhaled16@gmail.com>
2026-07-16 03:28:04 +0300

    Ticket Info Custom Widget

--- lib/features/confirmation/widget/tiecket_info_widget.dart
+++ lib/features/confirmation/widget/tiecket_info_widget.dart
@@ -1,3 +1,4 @@
+import 'package:aura_luxury_reservations/core/app_colors.dart';
 import 'package:aura_luxury_reservations/core/app_style.dart';
 import 'package:flutter/material.dart';
 
@@ -5,27 +6,20 @@ class TicketInfoWidget extends StatelessWidget {
   final String title;
   final String info;
 
-  const TicketInfoWidget({
-    super.key,
-    required this.title,
-    required this.info,
-  });
+  const TicketInfoWidget({super.key, required this.title, required this.info});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(
-          title,
-          style: AppStyle.bodySmall,
-        ),
+        Text(title, style: AppStyle.bodySmall),
 
         const SizedBox(height: 6),
 
         Text(
           info,
-          style: AppStyle.headlineMedium,
+          style: AppStyle.headlineMedium.copyWith(color: AppColors.primary),
           textAlign: TextAlign.center,
         ),
       ],
