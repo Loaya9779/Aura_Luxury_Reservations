9e8711f74a7f6af5f149e0873f0adfe26db692ea
Yasser <mr.yasser82@gmail.com>
2026-07-17 01:56:45 +0300

    feat: integrate flutter_screenutil for responsive design in my bookings screens

--- lib/features/my_bookings/widgets/book_history_row.dart
+++ lib/features/my_bookings/widgets/book_history_row.dart
@@ -1,6 +1,7 @@
 import 'package:aura_luxury_reservations/core/app_style.dart';
 import 'package:aura_luxury_reservations/features/my_bookings/widgets/fade_line.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BookHistoryRow extends StatelessWidget {
   const BookHistoryRow({super.key});
@@ -8,7 +9,7 @@ class BookHistoryRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(bottom: 30),
+      padding: EdgeInsets.only(bottom: 30.w),
       child: Row(
         children: [
           Expanded(child: FadeLine(isLeft: true)),
--- lib/features/my_bookings/widgets/my_booking_header.dart
+++ lib/features/my_bookings/widgets/my_booking_header.dart
@@ -1,5 +1,6 @@
 import 'package:aura_luxury_reservations/core/app_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyBookingHeader extends StatelessWidget {
   const MyBookingHeader({super.key});
@@ -7,7 +8,7 @@ class MyBookingHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RichText(
-      strutStyle: StrutStyle(height: 3),
+      strutStyle: StrutStyle(height: 3.h),
       text: TextSpan(
         children: [
           TextSpan(text: "My Bookings\n", style: AppStyle.headlineExtraLarge),
