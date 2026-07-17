ba1b5bb4f7f95cd796babc1a2223206743eab8dc
Loaya9779 <loayadel96@gmail.com>
2026-07-17 03:00:28 +0300

    add custom app bar

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:aura_luxury_reservations/core/app_colors.dart';
+import 'package:aura_luxury_reservations/core/app_style.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget? icon;
+  final bool isCenter;
+
+  const CustomAppBar({
+    super.key,
+    this.icon,
+    required this.isCenter,
+  });
+
+  @override
+
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: AppBar(
+        automaticallyImplyLeading: true,
+        backgroundColor: AppColors.background,
+        surfaceTintColor: AppColors.background,
+        elevation: 0,
+        title: Text('LUMIÈRE', style: AppStyle.labelLarge),
+        centerTitle: isCenter,
+        leading: icon,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(50);
+}
